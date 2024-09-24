13da50eff8be2b10ff354cc79948ce341a210cb7
Danielle Lindblom <114356705+Danielle254@users.noreply.github.com>
2024-09-24 07:48:11 -0600

    Update Map.js

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ export default function Map(props) {
     
     return (
         <div className={props.animateOn === true ? "map" : "mapOff"}>
-            <iframe title="Google Maps"  loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:EioybmQgUGwgJiBDb3VydCBTdCwgQnJvb2tseW4sIE5ZIDExMjMxLCBVU0EiZiJkChQKEglXGRhcWFrCiRGyde_BuJTmUBIUChIJVxkYXFhawokRsnXvwbiU5lAaFAoSCVuuBfJYWsKJEQIicj09qGFVGhQKEgnhT8evWVrCiRFy1_bXvC3EPSIKDVQzPxgVOOnk0w&key=AIzaSyADaoH9FEmhNoC8x6rTJjRToDihABF-sFk"></iframe>
+            <iframe title="Google Maps"  loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:EioybmQgUGwgJiBDb3VydCBTdCwgQnJvb2tseW4sIE5ZIDExMjMxLCBVU0EiZiJkChQKEglXGRhcWFrCiRGyde_BuJTmUBIUChIJVxkYXFhawokRsnXvwbiU5lAaFAoSCVuuBfJYWsKJEQIicj09qGFVGhQKEgnhT8evWVrCiRFy1_bXvC3EPSIKDVQzPxgVOOnk0w&key=AIzaSyCMJWiS93Ei4SzflVXdimo44tqtsFaoEWo-sFk"></iframe>
         </div>
     )
 }
